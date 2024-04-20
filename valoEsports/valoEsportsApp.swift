ca3d75b4e3cb7a039e0e9b9762f4672e420f7c9e
Kartikey <kartikey.soni@designare.in>
2024-04-21 00:17:52 +0530

    Initial Commit

---
+++ valoEsports/valoEsportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  valoEsportsApp.swift
+//  valoEsports
+//
+//  Created by Designare Karthikey on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct valoEsportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ valoEsportsUITests/valoEsportsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  valoEsportsUITestsLaunchTests.swift
+//  valoEsportsUITests
+//
+//  Created by Designare Karthikey on 21/04/24.
+//
+
+import XCTest
+
+final class valoEsportsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
